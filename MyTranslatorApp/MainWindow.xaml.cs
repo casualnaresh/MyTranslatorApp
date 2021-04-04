406e0729ceb2bd7063145a7d7a31b37320938335
Naresh <casualnaresh@gmail.com>
2021-04-04 15:57:34 -0400

    Add project files.

---
+++ MyTranslatorApp/MainWindow.xaml.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Windows;
+using System.Net;
+using System.Net.Http;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace MyTranslatorApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // This sample uses the Cognitive Services subscription key for all services.
+        const string COGNITIVE_SERVICES_KEY = "89d0f1ee15404d4a8946d686075f71c4";//key generated from Azure Portal
+        // Endpoints for Translator
+        public static readonly string TEXT_TRANSLATION_API_ENDPOINT = "https://api.cognitive.microsofttranslator.com/{0}?api-version=3.0";
+        // An array of language codes
+        private string[] languageCodes;
+
+        // Dictionary to map language codes from friendly name (sorted case-insensitively on language name)
+        private SortedDictionary<string, string> languageCodesAndTitles =
+            new SortedDictionary<string, string>(Comparer<string>.Create((a, b) => string.Compare(a, b, true)));
+
+        // Global exception handler to display error message and exit
+        private static void HandleExceptions(object sender, UnhandledExceptionEventArgs args)
+        {
+            Exception e = (Exception)args.ExceptionObject;
+            MessageBox.Show("Caught " + e.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            System.Windows.Application.Current.Shutdown();
+        }
+        // MainWindow constructor
+        public MainWindow()
+        {
+            // Display a message if unexpected error is encountered
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(HandleExceptions);
+
+            if (COGNITIVE_SERVICES_KEY.Length != 32)
+            {
+                MessageBox.Show("One or more invalid API subscription keys.\n\n" +
+                    "Put your keys in the *_API_SUBSCRIPTION_KEY variables in MainWindow.xaml.cs.",
+                    "Invalid Subscription Key(s)", MessageBoxButton.OK, MessageBoxImage.Error);
+                System.Windows.Application.Current.Shutdown();
+            }
+            else
+            {
+                // Start GUI
+                InitializeComponent();
+                // Get languages for drop-downs
+                //GetLanguagesForTranslate();
+                // Populate drop-downs with values from GetLanguagesForTranslate
+                PopulateLanguageMenus();
+            }
+        }
+        //GET LANGUAGE CODES
+        private void GetLanguagesForTranslate()
+        {
+            // Send request to get supported language codes
+            string uri = String.Format(TEXT_TRANSLATION_API_ENDPOINT, "languages") + "&scope=translation";
+            WebRequest WebRequest = WebRequest.Create(uri);
+            WebRequest.Headers.Add("Accept-Language", "en");
+            WebResponse response = null;
+            // Read and parse the JSON response
+            response = WebRequest.GetResponse();
+            using (var reader = new StreamReader(response.GetResponseStream(), UnicodeEncoding.UTF8))
+            {
+                var result = JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, Dictionary<string, string>>>>(reader.ReadToEnd());
+                var languages = result["translation"];
+
+                languageCodes = languages.Keys.ToArray();
+                foreach (var kv in languages)
+                {
+                    languageCodesAndTitles.Add(kv.Value["name"], kv.Key);
+                }
+            }
+        }
+        //Populate list of items in to combo boxes
+        private void PopulateLanguageMenus()
+        {
+            //int count = languageCodesAndTitles.Count;
+            //foreach (string menuItem in languageCodesAndTitles.Keys)
+            //{
+                FromLanguageComboBox.Items.Add("English");
+                ToLanguageComboBox.Items.Add("French");
+            //}
+
+            // Set default languages
+            FromLanguageComboBox.SelectedItem = "English";
+            ToLanguageComboBox.SelectedItem = "French";
+        }
+        //TRANSLATION OF INPUT TEXT ON BUTTON CLICK
+        private async void TranslateButton_Click(object sender, EventArgs e)
+        {
+            string textToTranslate = TextToTranslate.Text.Trim();
+
+            string fromLanguage = FromLanguageComboBox.SelectedValue.ToString();
+            string fromLanguageCode = "en";// languageCodesAndTitles[fromLanguage];
+
+            string toLanguageCode = "fr";// languageCodesAndTitles[ToLanguageComboBox.SelectedValue.ToString()];
+
+            // handle null operations: no text or same source/target languages
+            if (textToTranslate == "" || fromLanguageCode == toLanguageCode)
+            {
+                TranslatedTextLabel.Content = textToTranslate;
+                return;
+            }
+
+            // send HTTP request to perform the translation
+            string endpoint = string.Format(TEXT_TRANSLATION_API_ENDPOINT, "translate");
+            string uri = string.Format(endpoint + "&from={0}&to={1}", fromLanguageCode, toLanguageCode);
+
+            System.Object[] body = new System.Object[] { new { Text = textToTranslate } };
+            var requestBody = JsonConvert.SerializeObject(body);
+
+            using (var client = new HttpClient())
+            using (var request = new HttpRequestMessage())
+            {
+                request.Method = HttpMethod.Post;
+                request.RequestUri = new Uri(uri);
+                request.Content = new StringContent(requestBody, Encoding.UTF8, "application/json");
+                request.Headers.Add("Ocp-Apim-Subscription-Key", COGNITIVE_SERVICES_KEY);
+                request.Headers.Add("Ocp-Apim-Subscription-Region", "canadacentral");
+                request.Headers.Add("X-ClientTraceId", Guid.NewGuid().ToString());
+
+                var response = await client.SendAsync(request);
+                var responseBody = await response.Content.ReadAsStringAsync();
+
+                var result = JsonConvert.DeserializeObject<List<Dictionary<string, List<Dictionary<string, string>>>>>(responseBody);
+                var translation = result[0]["translations"][0]["text"];
+
+                // Update the translation field
+                TranslatedTextLabel.Content = translation;
+            }
+        }
+    }
+}
